228519150e35d1c6e7afbddbb4d40c41feff116a
Aleksandr Mishin <mishin.aa@phystech.edu>
2026-01-18 17:40:49 +0300

    inversion analysis
    Signed-off-by: Aleksandr Mishin <mishin.aa@phystech.edu>

--- src/analysers/top_dependencies.py
+++ src/analysers/top_dependencies.py
@@ -9,13 +9,16 @@ class DependenciesLister:
     @classmethod
     def run_analysis(cls, input_folder: Path, output_dir: Path):
         dfs_lst = []
+        dfs_inv_lst = []
         for cur_file in input_folder.iterdir():
             df = pd.read_csv(cur_file)
             df = cls.drop_banned_dependencies(df)
             dfs_lst.append(cls.get_top_dependencies(df))
-
+            dfs_inv_lst.append(cls.get_top_inv_dependencies(df))
         res_df = pd.concat(dfs_lst).groupby("waker_waiter_pair", as_index=False).sum()
-        cls.plot_dependencies(res_df, output_dir)
+        res_inv_df = pd.concat(dfs_inv_lst).groupby("waker_waiter_pair", as_index=False).sum()
+        cls.plot_dependencies(res_df, output_dir, "top_depedencies.png", "Overall statistics")
+        cls.plot_dependencies(res_inv_df, output_dir, "top_capacity_inversion_depedencies.png", "Inversion related statistics")
 
     @classmethod
     def drop_banned_dependencies(cls, df: pd.DataFrame) -> pd.DataFrame:
@@ -44,7 +47,31 @@ class DependenciesLister:
         return res_df
 
     @classmethod
-    def plot_dependencies(cls, df: pd.DataFrame, output_dir: Path):
+    def get_top_inv_dependencies(cls, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        returns top by amount and top by wait time
+        dependencies when inversion happened
+        """
+
+        wdf = df.copy()
+
+        unknown_core_mask = wdf["wait_start_waker_freq"].astype(int) == -1
+        cap_inversion_mask = wdf["wait_start_waker_freq"].astype(int) < wdf["wait_start_waiter_freq"].astype(int)
+        wdf = wdf[cap_inversion_mask & (~unknown_core_mask)].copy()
+
+        wdf["waker_info"] = wdf["waker_comm"] + "-" + wdf["waker_pid"].astype(str) 
+        wdf["waiter_info"] = wdf["waiter_comm"] + "-" + wdf["waiter_pid"].astype(str) 
+        wdf["waker_waiter_pair"] = wdf["waker_info"] + "_" + wdf["waiter_info"]
+
+        res_df = wdf.groupby("waker_waiter_pair").agg(
+            total_duration_us=("waiting_duration_us", "sum"),
+            total_dependencies=("waiting_duration_us", "count")
+        ).reset_index()
+
+        return res_df
+
+    @classmethod
+    def plot_dependencies(cls, df: pd.DataFrame, output_dir: Path, output_name: str, huge_title: str):
         output_dir.mkdir(exist_ok=True, parents=True)
 
         n_dependencies = 30
@@ -65,5 +92,6 @@ class DependenciesLister:
             ax.set_title(f"top {n_dependencies} dependencies by {title}")
             ax.grid(True, axis="x", alpha=0.3)
 
+        plt.suptitle(huge_title)
         plt.tight_layout()
-        plt.savefig(output_dir/"top_depedencies.png")
+        plt.savefig(output_dir / output_name)
